e41e086e214b290b4c37bef61aa8bf43eefe7580
Ory-Khoiri <25oryk@gmail.com>
2023-01-29 21:06:37 +0000

    second stage

---
+++ src/SimpleBot.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.print("Please, remind me your name.");
+        String yourName = scanner.next();
+
+        System.out.println("What a great name you have, " + yourName + "!");
+    }
+}
