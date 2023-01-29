7880ea56d1c0f069f72623d951587b0be1fe90b8
Ory-Khoiri <25oryk@gmail.com>
2023-01-29 20:54:19 +0000

    first stage

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello! My name is Melinda.\n" +
+                "I was created in 2023.");
     }
 }
