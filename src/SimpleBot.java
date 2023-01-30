08db469e9712f941a7b7f8f55117de18829773b1
Ory-Khoiri <25oryk@gmail.com>
2023-01-30 16:24:40 +0000

    final stage commit

--- src/SimpleBot.java
+++ src/SimpleBot.java
@@ -2,9 +2,18 @@
 import java.util.Scanner;
 
 public class SimpleBot {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+    final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        greeting();
+        guessAge();
+        counting();
+        test();
+        end();
+
+    }
+    static void greeting(){
         //Chatbot conversation introduction
         System.out.println("Hello! My name is Aid.");
         System.out.println("I was created in 2018.");
@@ -13,6 +22,9 @@ public class SimpleBot {
         // string formatting using %s
         System.out.printf("What a great name you have, %s! \n", yourName);
 
+    }
+
+    static void guessAge(){
         // guess age game
         System.out.println("Let me guess your age.");
         System.out.println("Enter reminder of dividing your age by 3, 5, 7");
@@ -27,6 +39,9 @@ public class SimpleBot {
         //string formatting using %d
         System.out.printf("Your age is %d, that's a good time to start programming! \n", age);
 
+    }
+
+    static void counting(){
         // counting number ability
         System.out.println("Now I will prove to you that I can count to any number you want.");
         int number = scanner.nextInt();// input number
@@ -35,9 +50,31 @@ public class SimpleBot {
             System.out.printf("%d!\n", i); // print loop 0 <= input number
         }
 
-        System.out.println("Completed, have a nice day!");
+    }
 
+    static void test(){
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Why do we used method?");
+        System.out.println("1. To repeat a statement multiple times.");
+        System.out.println("2. To decompose a program into several small subroutines.");
+        System.out.println("3. To determine the execution time of a program.");
+        System.out.println("4. To interrupt the execution of a program.");
 
+        int answer = 0;
 
+        while (answer != 2) {
+            answer = scanner.nextInt();
+            if (answer == 2) {
+                break;
+            } else {
+                System.out.println("Please, try again.");
+
+            }
+        }
+
+    }
+
+    static void end(){
+        System.out.println("Congratulations, have a nice day!\n");
     }
 }
